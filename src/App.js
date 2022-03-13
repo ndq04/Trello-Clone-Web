5bc2868c494d0ea455d75e068c029e9e0b70ac55
ndq04 <duyquang412000@gmail.com>
2022-03-13 18:30:16 +0700

    Design trello front-end

--- src/App.js
+++ src/App.js
@@ -1,9 +1,81 @@
-import './App.css'
+import './App.scss'
 
 function App() {
   return (
-    <div className='App'>
-      <p>hello world!</p>
+    <div className='trello-master'>
+      <nav className='navbar app'>App Bar</nav>
+      <nav className='navbar board'>Board Bar</nav>
+      <div className='board-columns'>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src='https://res.cloudinary.com/doltvro6d/image/upload/v1644809834/qsocial/at6wdjb02h0kcnojwbv4.jpg'
+                alt='duyquang-dev'
+              />
+              Title: DUYQUANGDEV
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src='https://res.cloudinary.com/doltvro6d/image/upload/v1644809834/qsocial/at6wdjb02h0kcnojwbv4.jpg'
+                alt='duyquang-dev'
+              />
+              Title: DUYQUANGDEV
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src='https://res.cloudinary.com/doltvro6d/image/upload/v1644809834/qsocial/at6wdjb02h0kcnojwbv4.jpg'
+                alt='duyquang-dev'
+              />
+              Title: DUYQUANGDEV
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   )
 }
